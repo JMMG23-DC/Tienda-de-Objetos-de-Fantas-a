54fd74de09415c081614331c70a0ede36e5f09c0
Adrian <afloresu.01@gmail.com>
2025-09-25 22:10:36 -0500

    Comandos de git

---
+++ Subir_tu_branch.bat
@@ -0,0 +1,6 @@
+@echo off
+git checkout <"Poner el nomnre de tu BRANCH>"
+git add <"Nombre del Archivo que quieres subir o Actualizar Ejemplo main.py"
+git commit -m "Actualizar <Nombre del Archivo>"
+git push origin Adrian
+
--- git_comands.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git checkout Adrian
-git add README.md
-git commit -m "Actualizar Readme"
-git push origin Adrian
