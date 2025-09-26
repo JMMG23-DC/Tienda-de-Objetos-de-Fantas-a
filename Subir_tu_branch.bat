b84fcf484032ed57ccfb19ca3325f5574e00dc83
Adrian <afloresu.01@gmail.com>
2025-09-25 22:21:07 -0500

    Inicializando React+Vite

--- Subir_tu_branch.bat
+++ Subir_tu_branch.bat
@@ -1,6 +1,5 @@
 @echo off
-git checkout <"Poner el nomnre de tu BRANCH>"
-git add <"Nombre del Archivo que quieres subir o Actualizar Ejemplo main.py"
-git commit -m "Actualizar <Nombre del Archivo>"
-git push origin Adrian
-
+git checkout main
+git add .
+git commit -m "Inicializando React+Vite"
+git push origin main
